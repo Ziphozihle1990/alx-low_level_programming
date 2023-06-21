dd44e2e9ad77ba477cc591bf9e9d47fbdd55b97c
Ziphozihle1990 <ziphozihlembilase@yahoo.com>
2023-06-21 14:21:40 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,7 +6,7 @@
  * Return: 1 for lowercase character or 0 for anything else
  */
 
-int _islower(int c)
+int _is_lower(int c)
 
 {
 
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,16 @@
 
 /**
  * print_sign - prints the sign of a number
- *@n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for anything else
+ *@n: is the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is 
  */
 
-int print_sign(int c)
-
+int _islower(int c)
 {       
+        if (c >= 'a' && c <= 'z')
+                return (1);
+        else
+                return (0);
 
-	if (n > 0)
-	{
-	_putchar(43);
-	return (1);
-	}
-	else if (n < 0)
-	{
-	_putchar(45);
-	return (-1);
-	}
-	else
-	{
-	_putchar(48);
-	return (0);
-	}
-}
+
