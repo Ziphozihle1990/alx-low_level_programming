52f15e1409ba548f09181c46aea1feb3af1f1ecd
Ziphozihle1990 <ziphozihlembilase@yahoo.com>
2023-06-21 17:55:09 -0700

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,14 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <=n9; num ++)
+	for (num = 0; num <= 9; num ++)
 	{
 		_putchar('0');
 
-		for (mult + 1; mult <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putchar('');
+			_putchar(' ');
 			prod = num * mult;
 			if (prod <= 9)
 _putchar(' ');
