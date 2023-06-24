95a71488cc6a3c33bde6226421c4157316e43e78
Ziphozihle1990 <ziphozihlembilase@yahoo.com>
2023-06-23 19:51:16 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,15 @@
 
 /**
  * _isdigit - Check if numbers are 0 - 9
- *@c: cha to be checked
+ *@c: char to be checked
  *
  *Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
-       if(c >= '0' && c <= '9')
-	       return (1);
-       else
-	       return (0);
-}       
+	if(c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
