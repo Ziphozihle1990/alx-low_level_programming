0da9b07d47c13196f93486c8e4e11fa6647c4ad3
Ziphozihle1990 <ziphozihlembilase@yahoo.com>
2023-06-26 16:21:15 -0700

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,5 +14,4 @@ void print_array(int *a, int n);
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 
-
-#endif /
+#endif
