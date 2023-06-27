67b7a55419439d18379a6329964e083d8d0008c8
Ziphozihle1990 <ziphozihlembilase@yahoo.com>
2023-06-26 18:31:24 -0700

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,11 +5,12 @@
  * Return: String in reverse
  */
 void rev_string(char *s)
-{	
+{
 	char rev = s[0];
 	int counter = 0;
 	int i;
 	while (s[counter] != '\0');
+
 	counter++;
 	for (i = 0; i < counter; i++)
 	{
